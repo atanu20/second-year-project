72e562fb88a7a0e246dfb4475497f07750c1221c
atanu20 <65564188+atanu20@users.noreply.github.com>
2020-10-04 21:19:25 +0530

    second-year-project
    second-year-project

---
+++ src/Searchimage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Searchimage=(props)=>{
+    const image=`https://source.unsplash.com/400x300/?${props.image}`;
+
+
+    return(<>
+    <div className="d-flex justify-content-center">
+
+    <img src={image} alt="search image"/>
+    </div>
+
+    
+    
+    
+    
+    
+    </>);
+
+
+
+}
+
+export default Searchimage;
