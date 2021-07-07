781beec5e6d6c80207a87b68836561b873640d6a
ATANU JANA <65564188+atanu20@users.noreply.github.com>
2021-07-07 20:02:11 +0530

    Update Searchimage.jsx

--- food-recipe-in react-using-api/src/Searchimage.jsx
+++ food-recipe-in react-using-api/src/Searchimage.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 const Searchimage=(props)=>{
     const image=`https://source.unsplash.com/400x300/?${props.image}`;
-
-
     return(<>
     <div className="d-flex justify-content-center">
 
